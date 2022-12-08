0af7712446415c37f36bead05b2a944e7fbb470d
Hector Romero <harelehg@gmail.com>
2022-12-07 22:20:38 -0600

    Actualizacion de la ruta de los script y se agrego la pagina de busqueda de docentes y estudiantes

---
+++ assets/js/script_search.js
@@ -0,0 +1,17 @@
+document.addEventListener("keyup", e=>{
+
+    if (e.target.matches("#buscador")){
+  
+        if (e.key ==="Escape")e.target.value = ""
+  
+        document.querySelectorAll(".articulo").forEach(datos =>{
+  
+            datos.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+              ?datos.classList.remove("filtro")
+              :datos.classList.add("filtro")
+        })
+  
+    }
+  
+  
+  })
